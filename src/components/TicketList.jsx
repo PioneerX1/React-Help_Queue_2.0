44abda7f68fc71fe9d8d508c74d4f846a2e183ef
Mick Sexton <lacrookedbeat@hotmail.com>
2018-01-31 22:27:03 -0800

    setup React with project folder structure and add Hot Mod Replacement for quick deployment

---
+++ src/components/TicketList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Ticket from "./Ticket";
+
+function TicketList(){
+  return(
+    <Ticket
+      location="3A"
+      names="Lynn and Anna K"
+      issue="Firebase won't fire my base!"/>
+  );
+}
+
+export default TicketList;
