94b7992222901ab7e204fdbfb00057b694c93207
Mick Sexton <lacrookedbeat@hotmail.com>
2018-02-03 16:53:20 -0800

    add NewTicketForm component and React-Router to project

---
+++ src/components/NewTicketForm.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function NewTicketForm(){
+
+  return(
+    <div>
+      <form>
+        <input
+          type='text'
+          id='names'
+          placeholder='Pair Names'
+          />
+        <input
+          type='text'
+          id='location'
+          placeholder='Location'
+          />
+        <input
+          type='text'
+          id='issue'
+          placeholder='Describe your issue.'
+          />
+        <button type='submit'>Help!</button>
+      </form>
+    </div>
+  );
+}
+
+export default NewTicketForm;
