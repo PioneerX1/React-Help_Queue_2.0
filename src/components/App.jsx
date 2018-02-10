d84ad06bbb5008ffefa6c5537fccdb53b5f75b12
Mick Sexton <lacrookedbeat@hotmail.com>
2018-02-10 14:50:47 -0800

    add moment js to calculate wait times of tickets, display, and update ui

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import Header from './Header';
 import TicketList from './TicketList';
 import NewTicketControl from './NewTicketControl';
 import Error404 from './Error404';
+import Moment from 'moment';
 
 class App extends React.Component {
 
@@ -17,9 +18,32 @@ class App extends React.Component {
 
   handleAddingNewTicketToList(newTicket){
     var newMasterTicketList = this.state.masterTicketList.slice();
+    newTicket.formattedWaitTime = (newTicket.timeOpen).fromNow(true);
     newMasterTicketList.push(newTicket);
     this.setState({masterTicketList: newMasterTicketList});
   }
+
+  componentWillMount() {
+    console.log('componentWillMount');
+  }
+
+  componentWillReceiveProps() {
+    console.log('componentWillReceiveProps');
+  }
+
+  shouldComponentUpdate() {
+    console.log('shouldComponentUpdate');
+    return true;
+  }
+
+  componentWillUpdate() {
+    console.log('componentWillUpdate');
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate');
+  }
+
 
   render(){
     return (
@@ -32,6 +56,20 @@ class App extends React.Component {
         </Switch>
       </div>
     );
+  }
+  componentDidMount() {
+    this.waitTimeUpdateTimer = setInterval(() => this.updateTicketElapsedWaitTime(), 60000);
+  }
+
+  updateTicketElapsedWaitTime() {
+    console.log('check');
+    let newMasterTicketList = this.state.masterTicketList.slice();
+    newMasterTicketList.forEach((ticket) => ticket.formattedWaitTime = (ticket.timeOpen).fromNow(true));
+    this.setState({masterTicketList: newMasterTicketList})
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.waitTimeUpdateTimer);
   }
 }
 
--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+
 function Ticket(props){
   var ticketStyles = {
     backgroundColor: 'salmon',
     fontStyle: 'bold',
     paddingLeft: 20
   };
-  
+
   return(
     <div style={ticketStyles}>
       <h3>{props.location} - {props.names}</h3>
-      <p><em>{props.issue}</em></p>
+      <h4><em>{props.formattedWaitTime}</em></h4>
+      <p>{props.issue}</p>
       <hr/>
     </div>
   );
 }
 
+
 Ticket.propTypes = {
   names: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
-  issue: PropTypes.string
+  issue: PropTypes.string,
+  formattedWaitTime: PropTypes.string.isRequired,
 };
 
 export default Ticket;
